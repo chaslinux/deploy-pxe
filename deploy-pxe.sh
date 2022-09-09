5335d7367142d12b88356458c6159c650bcdf51c
chaslinux <chaslinux@gmail.com>
2022-09-09 19:35:24 -0400

    Added echo statements for human consumption.

--- deploy-pxe.sh
+++ deploy-pxe.sh
@@ -21,19 +21,22 @@ sudo apt update && sudo apt upgrade -y
 # Install syslinux for non-UEFI and UEFI, plus tftpd
 # We don't install a DHCP server since we're using our router
 # for dhcp
-echo "Installing syslinux-common, syslinux-efi, tftpd-hpa, pxelinux, and apache2"
+echo "Installing syslinux-common, syslinux-efi, tftpd-hpa, pxelinux, and apache2..."
 sudo apt install syslinux-common syslinux-efi tftpd-hpa pxelinux apache2 -y
 
+echo "Copying syslinux and pxelinux files into the appropriate directories..."
 cd /srv/tftp
 sudo cp /usr/lib/syslinux/modules/efi32/ldlinux.e32 /srv/tftp
 sudo cp /usr/lib/syslinux/modules/efi64/{ldlinux.e64,libutil.c32,menu.c32} /srv/tftp
 sudo cp /usr/lib/SYSLINUX.EFI/efi64/syslinux.efi /srv/tftp
 sudo cp /usr/lib/PXELINUX/pxelinux.0 /srv/tftp
 
+echo "Make the pxelinux.cfg directory and add a blank default file..."
 sudo mkdir -p /srv/tftp/pxelinux.cfg
 sudo touch /srv/tftp/pxelinux.cfg/default
 
 # make the distribution directories
+echo "Make directories to hold Debian, Ubuntu server/desktop, Xubuntu desktop, and Fedora server/desktop..."
 sudo mkdir -p /srv/tftp/debian/bullseye
 sudo mkdir -p /srv/tftp/ubuntu/jammy/{server,desktop}
 sudo mkdir -p /srv/tftp/xubuntu/jammy/desktop
@@ -41,54 +44,66 @@ sudo mkdir -p /srv/tftp/fedora/36/{server,desktop}
 
 
 # change to the current user home directory
+echo "Downloading Ubuntu Server 22.04..."
 cd ~
 wget https://releases.ubuntu.com/22.04.1/ubuntu-22.04.1-live-server-amd64.iso
 
 # set up Ubuntu server software directory structure
+echo "Mounting Ubuntu Server image, copying vmlinuz, initrd, and the ISO to the appropriate directories..."
 sudo mount ubuntu-22.04.1-live-server-amd64.iso /mnt
 sudo cp /mnt/casper/{initrd,vmlinuz} /srv/tftp/ubuntu/jammy/server
 sudo mv ubuntu-22.04.1-live-server-amd64.iso /srv/tftp/ubuntu/jammy/server
 sudo umount /mnt
 
 # get Ubuntu Desktop
+echo "Downloading Ubuntu 22.04 desktop..."
 wget https://releases.ubuntu.com/22.04.1/ubuntu-22.04.1-desktop-amd64.iso
 
 # set up Ubuntu desktop directory structure
+echo "Mounting Ubuntu Desktop image, copying vmlinuz, initrd, and the ISO to the appropriate directories..."
 sudo mount ubuntu-22.04.1-desktop-amd64.iso /mnt
 sudo cp /mnt/casper/{initrd,vmlinuz} /srv/tftp/ubuntu/jammy/desktop
 sudo mv ubuntu-22.04.1-desktop-amd64.iso /srv/tftp/ubuntu/jammy/desktop
 sudo umount /mnt
 
 # get debian
+echo "Downloading the Debian 11.4.0 netinstall image"
 wget https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-11.4.0-amd64-netinst.iso
 
 # set up debian directory structure
+echo "Mounting Debian image, copying vmlinuz, initrd, and the ISO to the appropriate directories..."
 sudo mount debian-11.4.0-amd64-netinst.iso /mnt
 sudo cp /mnt/install.amd/{initrd.gz,vmlinuz} /srv/tftp/debian/bullseye
 sudo mv debian-11.4.0-amd64-netinst.iso /srv/tftp/debian/bullseye
 sudo umount /mnt
 
 # get xubuntu (desktop) - note Canadian mirror
+echo "Downloading the Xubuntu 22.04 desktop image from Canada, eh..."
 wget http://mirror.csclub.uwaterloo.ca/xubuntu-releases/22.04/release/xubuntu-22.04.1-desktop-amd64.iso
 
 # set up the Xubuntu directory structure
+echo "Mounting Xubuntu image, copying vmlinuz, initrd, and the ISO to the appropriate directories..."
 sudo mount xubuntu-22.04.1-desktop-amd64.iso /mnt
 sudo cp /mnt/casper/{initrd,vmlinuz} /srv/tftp/xubuntu/jammy/desktop
 sudo mv xubuntu-22.04.1-desktop-amd64.iso /srv/tftp/xubuntu/jammy/desktop
 sudo umount /mnt
 
 # get fedora (GNOME)
+echo "Downloading the Fedora Desktop 36-1.5 image..."
 wget https://download.fedoraproject.org/pub/fedora/linux/releases/36/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-36-1.5.iso
 
 # Set up the Fedora Desktop structure
+echo "Mounting Fedora Desktop image, copying vmlinuz, initrd, and the ISO to the appropriate directories..."
 sudo mount Fedora-Workstation-Live-x86_64-36-1.5.iso /mnt
 sudo cp /mnt/isolinux/{initrd.img,vmlinuz} /srv/tftp/fedora/36/desktop
 sudo mv Fedora-Workstation-Live-x86_64-36-1.5.iso /srv/tftp/fedora/36/desktop
 sudo umount /mnt
 
 # Get fedora (server)
+echo "Downloading Fedora Server 36 netinstall image..."
 wget https://download.fedoraproject.org/pub/fedora/linux/releases/36/Server/x86_64/iso/Fedora-Server-netinst-x86_64-36-1.5.iso
 
+echo "Mounting Fedora Server image, copying vmlinuz, initrd, and the ISO to the appropriate directories..."
 sudo mount Fedora-Server-netinst-x86_64-36-1.5.iso /mnt
 sudo cp /mnt/isolinux/{initrd.img,vmlinuz} /srv/tftp/fedora/36/server
 sudo mv Fedora-Server-netinst-x86_64-36-1.5.iso /srv/tftp/fedora/36/server
@@ -96,6 +111,7 @@ sudo umount /mnt
 
 
 # open port 69/udp on the local machine firewall
+echo "Opening UDP port 69 on local firewall..."
 sudo ufw allow 69/udp
 
 
