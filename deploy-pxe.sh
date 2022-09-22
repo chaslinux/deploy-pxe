e48b67c46f04fc5d858152d48a8a54132c77e6e9
chaslinux <chaslinux@gmail.com>
2022-09-21 20:18:59 -0400

    Commented out Debian and Fedora as these need more work. Also commented out pxelinux.cfg/default as it needs more work.

--- deploy-pxe.sh
+++ deploy-pxe.sh
@@ -12,7 +12,7 @@
 
 ### *** IMPORTANT *** ###
 
-# 09/09/2022
+# 09/21/2022
 # This script is NOT finished. It's just a start
 
 echo "Updating the system before adding software..."
@@ -26,21 +26,55 @@ sudo apt install syslinux-common syslinux-efi tftpd-hpa pxelinux apache2 -y
 
 echo "Copying syslinux and pxelinux files into the appropriate directories..."
 cd /srv/tftp
+sudo chown tftp:tftp /srv/tftp
 sudo cp /usr/lib/syslinux/modules/efi32/ldlinux.e32 /srv/tftp
 sudo cp /usr/lib/syslinux/modules/efi64/{ldlinux.e64,libutil.c32,menu.c32} /srv/tftp
 sudo cp /usr/lib/SYSLINUX.EFI/efi64/syslinux.efi /srv/tftp
 sudo cp /usr/lib/PXELINUX/pxelinux.0 /srv/tftp
 
-echo "Make the pxelinux.cfg directory and add a blank default file..."
+echo "Make the pxelinux.cfg directory and set up default file structure..."
 sudo mkdir -p /srv/tftp/pxelinux.cfg
 sudo touch /srv/tftp/pxelinux.cfg/default
+#sudo echo "default menu.c32" > /srv/tftp/pxelinux.cfg/default
+#sudo echo "MENU TITLE Main Menu" > /srv/tftp/pxelinux.cfg/default
+#sudo echo "LABEL Desktop Linux" > /srv/tftp/pxelinux.cfg/default
+#sudo echo "		MENU LABEL Desktop Linux" > /srv/tftp/pxelinux.cfg/default
+#sudo echo "		KERNEL menu.c32" > /srv/tftp/pxelinux.cfg/default
+#sudo echo "		APPEND pxelinux.cfg/desktop_linux" > /srv/tftp/pxelinux.cfg/default
+#sudo echo "LABEL Linux Servers" > /srv/tftp/pxelinux.cfg/default
+#sudo echo "		MENU LABEL Linux Servers" > /srv/tftp/pxelinux.cfg/default
+#sudo echo "		KERNEL menu.c32" > /srv/tftp/pxelinux.cfg/default
+#sudo echo "		APPEND pxelinux.cfg/linux_servers" > /srv/tftp/pxelinux.cfg/default
+#sudo echo "LABEL Tools" > /srv/tftp/pxelinux.cfg/default
+#sudo echo "		MENU LABEL Tools" > /srv/tftp/pxelinux.cfg/default
+#sudo echo "		KERNEL menu.c32" > /srv/tftp/pxelinux.cfg/default
+#sudo echo "		APPEND pxelinux.cfg/tools" > /srv/tftp/pxelinux.cfg/default
+
+#echo "Making the pxelinux.cfg/desktop_linux menu..."
+#sudo touch /srv/tftp/pxelinux.cfg/desktop_linux
+#sudo echo "MENU TITLE Desktop Linux" > /srv/tftp/pxelinux.cfg/desktop_linux
+#sudo echo "LABEL Main Menu" > /srv/tftp/pxelinux.cfg/desktop_linux
+#sudo echo "		MENU LABEL Main Menu" > /srv/tftp/pxelinux.cfg/desktop_linux
+#sudo echo "		KERNEL menu.c32" > /srv/tftp/pxelinux.cfg/desktop_linux
+#sudo echo "		APPEND pxelinux.cfg/default" > /srv/tftp/pxelinux.cfg/desktop_linux
+#sudo echo "LABEL Debian Netinstall" > /srv/tftp/pxelinux.cfg/desktop_linux
+#sudo echo "		MENU LABEL Debian Netinstall" > /srv/tftp/pxelinux.cfg/desktop_linux
+#sudo echo "		KERNEL debian/bullseye/vmlinuz" > /srv/tftp/pxelinux.cfg/desktop_linux
+#sudo echo "		INITRD debian/bullseye/initrd.gz" > /srv/tftp/pxelinux.cfg/desktop_linux
+# sudo echo "" > /srv/tftp/pxelinux.cfg/desktop_linux
+# sudo echo "" > /srv/tftp/pxelinux.cfg/desktop_linux
+# sudo echo "" > /srv/tftp/pxelinux.cfg/desktop_linux
+# sudo echo "" > /srv/tftp/pxelinux.cfg/desktop_linux
+# sudo echo "" > /srv/tftp/pxelinux.cfg/desktop_linux
+# sudo echo "" > /srv/tftp/pxelinux.cfg/desktop_linux
+
 
 # make the distribution directories
 echo "Make directories to hold Debian, Ubuntu server/desktop, Xubuntu desktop, and Fedora server/desktop..."
-sudo mkdir -p /srv/tftp/debian/bullseye
+#sudo mkdir -p /srv/tftp/debian/bullseye
 sudo mkdir -p /srv/tftp/ubuntu/jammy/{server,desktop}
 sudo mkdir -p /srv/tftp/xubuntu/jammy/desktop
-sudo mkdir -p /srv/tftp/fedora/36/{server,desktop}
+#sudo mkdir -p /srv/tftp/fedora/36/{server,desktop}
 
 
 # change to the current user home directory
@@ -67,15 +101,15 @@ sudo mv ubuntu-22.04.1-desktop-amd64.iso /srv/tftp/ubuntu/jammy/desktop
 sudo umount /mnt
 
 # get debian
-echo "Downloading the Debian 11.4.0 netinstall image"
-wget https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-11.4.0-amd64-netinst.iso
+#echo "Downloading the Debian 11.4.0 netinstall image"
+#wget https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-11.4.0-amd64-netinst.iso
 
 # set up debian directory structure
-echo "Mounting Debian image, copying vmlinuz, initrd, and the ISO to the appropriate directories..."
-sudo mount debian-11.4.0-amd64-netinst.iso /mnt
-sudo cp /mnt/install.amd/{initrd.gz,vmlinuz} /srv/tftp/debian/bullseye
-sudo mv debian-11.4.0-amd64-netinst.iso /srv/tftp/debian/bullseye
-sudo umount /mnt
+#echo "Mounting Debian image, copying vmlinuz, initrd, and the ISO to the appropriate directories..."
+#sudo mount debian-11.4.0-amd64-netinst.iso /mnt
+#sudo cp /mnt/install.amd/{initrd.gz,vmlinuz} /srv/tftp/debian/bullseye
+#sudo mv debian-11.4.0-amd64-netinst.iso /srv/tftp/debian/bullseye
+#sudo umount /mnt
 
 # get xubuntu (desktop) - note Canadian mirror
 echo "Downloading the Xubuntu 22.04 desktop image from Canada, eh..."
@@ -89,25 +123,25 @@ sudo mv xubuntu-22.04.1-desktop-amd64.iso /srv/tftp/xubuntu/jammy/desktop
 sudo umount /mnt
 
 # get fedora (GNOME)
-echo "Downloading the Fedora Desktop 36-1.5 image..."
-wget https://download.fedoraproject.org/pub/fedora/linux/releases/36/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-36-1.5.iso
+#echo "Downloading the Fedora Desktop 36-1.5 image..."
+#wget https://download.fedoraproject.org/pub/fedora/linux/releases/36/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-36-1.5.iso
 
 # Set up the Fedora Desktop structure
-echo "Mounting Fedora Desktop image, copying vmlinuz, initrd, and the ISO to the appropriate directories..."
-sudo mount Fedora-Workstation-Live-x86_64-36-1.5.iso /mnt
-sudo cp /mnt/isolinux/{initrd.img,vmlinuz} /srv/tftp/fedora/36/desktop
-sudo mv Fedora-Workstation-Live-x86_64-36-1.5.iso /srv/tftp/fedora/36/desktop
-sudo umount /mnt
+#echo "Mounting Fedora Desktop image, copying vmlinuz, initrd, and the ISO to the appropriate directories..."
+#sudo mount Fedora-Workstation-Live-x86_64-36-1.5.iso /mnt
+#sudo cp /mnt/isolinux/{initrd.img,vmlinuz} /srv/tftp/fedora/36/desktop
+#sudo mv Fedora-Workstation-Live-x86_64-36-1.5.iso /srv/tftp/fedora/36/desktop
+#sudo umount /mnt
 
 # Get fedora (server)
-echo "Downloading Fedora Server 36 netinstall image..."
-wget https://download.fedoraproject.org/pub/fedora/linux/releases/36/Server/x86_64/iso/Fedora-Server-netinst-x86_64-36-1.5.iso
+#echo "Downloading Fedora Server 36 netinstall image..."
+#wget https://download.fedoraproject.org/pub/fedora/linux/releases/36/Server/x86_64/iso/Fedora-Server-netinst-x86_64-36-1.5.iso
 
-echo "Mounting Fedora Server image, copying vmlinuz, initrd, and the ISO to the appropriate directories..."
-sudo mount Fedora-Server-netinst-x86_64-36-1.5.iso /mnt
-sudo cp /mnt/isolinux/{initrd.img,vmlinuz} /srv/tftp/fedora/36/server
-sudo mv Fedora-Server-netinst-x86_64-36-1.5.iso /srv/tftp/fedora/36/server
-sudo umount /mnt
+#echo "Mounting Fedora Server image, copying vmlinuz, initrd, and the ISO to the appropriate directories..."
+#sudo mount Fedora-Server-netinst-x86_64-36-1.5.iso /mnt
+#sudo cp /mnt/isolinux/{initrd.img,vmlinuz} /srv/tftp/fedora/36/server
+#sudo mv Fedora-Server-netinst-x86_64-36-1.5.iso /srv/tftp/fedora/36/server
+#sudo umount /mnt
 
 
 # open port 69/udp on the local machine firewall
